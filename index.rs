6b029b7eae65b41a1c838eb967a665c7d0e6c99a
Sven <bvssvni@gmail.com>
2013-12-13 06:51:30 +0100

    Added generic Group with operator overloading.

--- index.rs
+++ index.rs
@@ -1,38 +1,44 @@
 
 mod index {
-	pub fn or(a: &[int], b: &[int]) -> ~[int] {
+	pub fn or<T:Ord+Eq+Clone>(a: &[T], b: &[T]) -> ~[T] {
 		let mut i = 0;
 		let mut j = 1;
 		let an = a.len();
 		let bn = b.len();
-		let mut result: ~[int] = ~[];
+		let mut result: ~[T] = ~[];
 		while i < an || j < an {
 			let av = 
 				if i < an {
-					a[i]
+					&a[i]
 				} else {
-					::std::int::max_value
+					&a[an - 1]
 				};
 			let bv = 
 				if j < bn {
-					b[j]
+					&b[j]
 				} else {
-					::std::int::max_value
+					&b[bn - 1]
 				};
 			let min = 
-				if av < bv {
-					 av
-				} else {
+				if i >= an {
 					bv
+				} else if j >= bn {
+					av
+				} else {
+					if av < bv {
+						av
+					} else {
+						bv
+					}
 				};
-			if min == av {
+			if i < an && min == av {
 				i += 1;
 			}
-			if min == bv {
+			if j < bn && min == bv {
 				j += 1;
 			}
 
-			result.push(min);
+			result.push(min.clone());
 		}
 
 		result
@@ -46,24 +52,52 @@ mod index {
 		assert_eq!(c, ~[1, 2, 3, 4]);
 	}
 
-	pub struct Group {
-		members: ~[int],
+	pub struct Group<T> {
+		members: ~[T],
 	}
 
-	impl Add<Group, Group> for Group {
-		fn add(&self, rhs: &Group) -> Group {
+	pub trait BooleanAlgebra<T> {
+		fn or(&self, b: T) -> T;
+		fn and(&self, b: T) -> T;
+		fn except(&self, b: T) -> T;
+	}
+
+	impl<T:Ord+Eq+Clone> BooleanAlgebra<Group<T>> for Group<T> {
+		fn or(&self, b: Group<T>) -> Group<T> {
+			Group { members: self::or(self.members, b.members) }
+		}
+		fn and(&self, b: Group<T>) -> Group<T> {
+			Group { members: self::and(self.members, b.members) }
+		}
+		fn except(&self, b: Group<T>) -> Group<T> {
+			Group { members: self::except(self.members, b.members) }
+		}
+	}
+
+	pub trait IndexOf<T> {
+		fn index_of(&self, item: T) -> int;
+	}
+
+	impl<T:Ord+Eq> IndexOf<T> for Group<T> {
+		fn index_of(&self, item: T) -> int {
+			self::index_of(self.members, item)
+		}
+	}
+
+	impl<T:Ord+Eq+Clone> Add<Group<T>, Group<T>> for Group<T> {
+		fn add(&self, rhs: &Group<T>) -> Group<T> {
 			Group { members: or(self.members, rhs.members) }
 		}
 	}
 
-	impl Mul<Group, Group> for Group {
-		fn mul(&self, rhs: &Group) -> Group {
+	impl<T:Ord+Eq+Clone> Mul<Group<T>, Group<T>> for Group<T> {
+		fn mul(&self, rhs: &Group<T>) -> Group<T> {
 			Group { members: and(self.members, rhs.members) }
 		}
 	}
 
-	impl Sub<Group, Group> for Group {
-		fn sub(&self, rhs: &Group) -> Group {
+	impl<T:Ord+Eq+Clone> Sub<Group<T>, Group<T>> for Group<T> {
+		fn sub(&self, rhs: &Group<T>) -> Group<T> {
 			Group { members: except(self.members, rhs.members) }
 		}
 	}
@@ -74,6 +108,8 @@ mod index {
 		let b = Group { members: ~[2, 3, 4]} ;
 		let c = a + b;
 		assert_eq!(c.members, ~[1, 2, 3, 4]);
+		let d = a.or(b);
+		assert_eq!(d.members, ~[1, 2, 3, 4]);
 	}
 
 	#[test]
@@ -82,6 +118,8 @@ mod index {
 		let b = Group { members: ~[2, 3, 4] };
 		let c = a * b;
 		assert_eq!(c.members, ~[2, 3]);
+		let d = a.and(b);
+		assert_eq!(d.members, ~[2, 3]);
 	}
 
 	#[test]
@@ -90,6 +128,8 @@ mod index {
 		let b = Group { members: ~[2, 3, 4] };
 		let c = a - b;
 		assert_eq!(c.members, ~[1]);
+		let d = a.except(b);
+		assert_eq!(d.members, ~[1]);
 	}
 
 	#[test]
@@ -101,15 +141,15 @@ mod index {
 		assert_eq!(d.members, ~[]);
 	}
 
-	fn and(a: &[int], b: &[int]) -> ~[int] {
+	fn and<T:Ord+Eq+Clone>(a: &[T], b: &[T]) -> ~[T] {
 		let mut i = 0;
 		let mut j = 0;
 		let an = a.len();
 		let bn = b.len();
-		let mut result: ~[int] = ~[];
+		let mut result: ~[T] = ~[];
 		while i < an && j < bn {
-			let av = a[i];
-			let bv = b[j];
+			let av = &a[i];
+			let bv = &b[j];
 			let min =
 				if av < bv {
 					av
@@ -126,7 +166,7 @@ mod index {
 			}
 
 			if isAv && isBv {
-				result.push(min);
+				result.push(min.clone());
 			}
 		}
 
@@ -141,36 +181,40 @@ mod index {
 		assert_eq!(c, ~[2, 3]);
 	}
 
-	fn except(a: &[int], b: &[int]) -> ~[int] {
+	fn except<T:Ord+Eq+Clone>(a: &[T], b: &[T]) -> ~[T] {
 		let mut i = 0;
 		let mut j = 0;
 		let an = a.len();
 		let bn = b.len();
-		let mut result: ~[int] = ~[];
+		let mut result: ~[T] = ~[];
 		while i < an {
-			let av = a[i];
+			let av = &a[i];
 			let bv =
 				if j < bn {
-					b[j]
+					&b[j]
 				} else {
-					::std::int::max_value
+					&b[bn - 1]
 				};
 			let min =
-				if av < bv {
+				if j >= bn {
 					av
 				} else {
-					bv
+					if av < bv {
+						av
+					} else {
+						bv
+					}
 				};
 			let isAv = min == av;
 			let isBv = min == bv;
 			if isAv {
 				i += 1;
 			}
-			if isBv {
+			if j < bn && isBv {
 				j += 1;
 			}
 			if isAv && !isBv {
-				result.push(min);
+				result.push(min.clone());
 			}
 		}
 
@@ -185,7 +229,7 @@ mod index {
 		assert_eq!(c, ~[1]);
 	}
 
-	fn indexOf(list: &[int], item: int) -> int {
+	fn index_of<T:Ord+Eq>(list: &[T], item: T) -> int {
 		let mut low: int = 0;
 		let mut high: int = list.len() as int - 1;
 		while low <= high {
@@ -206,16 +250,30 @@ mod index {
 	}
 
 	#[test]
-	fn test_indexOf() {
+	fn test_index_of() {
 		let a: ~[int] = ~[1, 5, 10];
-		let b = indexOf(a, 5);
+		let b = index_of(a, 5);
 		assert_eq!(b, 1);
 	}
 
 	#[test]
-	fn test2_indexOf() {
+	fn test_index_of_group() {
+		let a = Group { members: ~[1, 5, 10] };
+		let b = a.index_of(5);
+		assert_eq!(b, 1);
+	}
+
+	#[test]
+	fn test2_index_of() {
 		let a: ~[int] = ~[1, 2, 10];
-		let b = indexOf(a, 0);
+		let b = index_of(a, 0);
+		assert_eq!(b, -1);
+	}
+
+	#[test]
+	fn test2_index_of_group() {
+		let a = Group { members: ~[1, 2, 10] };
+		let b = a.index_of(0);
 		assert_eq!(b, -1);
 	}
 }
