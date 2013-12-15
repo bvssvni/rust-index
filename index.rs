7f04c52f60b1ae10c964d86463d302c8bf9cbb9f
Sven <bvssvni@gmail.com>
2013-12-15 15:35:30 +0100

    Implemented proper Rust idioms.

--- index.rs
+++ index.rs
@@ -1,36 +1,59 @@
 
 mod index {
+	#[deriving(Eq)]
+	pub enum IndexMatch {
+		Found(int),
+		FoundLarger(int)
+	}
+
 	pub struct Index<T> {
 		priv ids: ~[T],
 	}
 
-	pub fn new<T>(data: ~[T]) -> Index<T> {
-		Index { ids: data }
+	impl<T: Clone + Ord + Eq> Index<T> {
+		pub fn new(data: ~[T]) -> Index<T> { Index { ids: data } }
+		pub fn to_vec(&self) -> ~[T] { self.ids.clone() }
+		pub fn index_of(&self, item: T) -> IndexMatch {
+			let list = &self.ids;
+			let mut low: int = 0;
+			let mut high: int = list.len() as int - 1;
+			while low <= high {
+				let i = (low + high) / 2;
+				if list[i] < item {
+					low = i + 1;
+					continue;
+				}
+				if list[i] > item {
+					high = i - 1;
+					continue;
+				}			
+
+				return Found(i);
+			}
+
+			FoundLarger(low)
+		}
 	}
 
-	pub fn to_vec<T:Clone>(a: Index<T>) -> ~[T] {
-		a.ids.clone()
-	}
-
-	impl<T:Ord+Eq+Clone> Add<Index<T>, Index<T>> for Index<T> {
+	impl<T: Ord + Eq + Clone> Add<Index<T>, Index<T>> for Index<T> {
 		fn add(&self, rhs: &Index<T>) -> Index<T> {
 			Index { ids: or(self.ids, rhs.ids) }
 		}
 	}
 
-	impl<T:Ord+Eq+Clone> Mul<Index<T>, Index<T>> for Index<T> {
+	impl<T: Ord + Eq + Clone> Mul<Index<T>, Index<T>> for Index<T> {
 		fn mul(&self, rhs: &Index<T>) -> Index<T> {
 			Index { ids: and(self.ids, rhs.ids) }
 		}
 	}
 
-	impl<T:Ord+Eq+Clone> Sub<Index<T>, Index<T>> for Index<T> {
+	impl<T: Ord + Eq + Clone> Sub<Index<T>, Index<T>> for Index<T> {
 		fn sub(&self, rhs: &Index<T>) -> Index<T> {
 			Index { ids: except(self.ids, rhs.ids) }
 		}
 	}
 
-	fn or<T:Ord+Eq+Clone>(a: &[T], b: &[T]) -> ~[T] {
+	fn or<T: Ord + Eq + Clone>(a: &[T], b: &[T]) -> ~[T] {
 		let mut i = 0;
 		let mut j = 1;
 		let an = a.len();
@@ -145,102 +168,75 @@ mod index {
 
 		result
 	}
-
-	#[deriving(Eq)]
-	pub enum IndexOf {
-		Found(int),
-		Next(int)
-	}
-	
-	pub fn index_of<T:Ord+Eq>(ind: Index<T>, item: T) -> IndexOf {
-		let list = ind.ids;
-		let mut low: int = 0;
-		let mut high: int = list.len() as int - 1;
-		while low <= high {
-			let i = (low + high) / 2;
-			if list[i] < item {
-				low = i + 1;
-				continue;
-			}
-			if list[i] > item {
-				high = i - 1;
-				continue;
-			}			
-
-			// return i;
-			return Found(i);
-		}
-
-		// -(low + 1)
-		Next(low)
-	}
 }
 
 #[cfg(test)]
 mod tests {
+	use index::Index;
+
 	#[test]
 	fn test_or_operator() {
-		let a = ::index::new( ~[1, 2, 3] );
-		let b = ::index::new( ~[2, 3, 4] );
+		let a = Index::new( ~[1, 2, 3] );
+		let b = Index::new( ~[2, 3, 4] );
 		let c = a + b;
-		assert_eq!(::index::to_vec(c), ~[1, 2, 3, 4]);
+		assert_eq!(c.to_vec(), ~[1, 2, 3, 4]);
 	}
 
 	#[test]
 	fn test_and_operator() {
-		let a = ::index::new( ~[1, 2, 3] );
-		let b = ::index::new( ~[2, 3, 4] );
+		let a = Index::new( ~[1, 2, 3] );
+		let b = Index::new( ~[2, 3, 4] );
 		let c = a * b;
-		assert_eq!(::index::to_vec(c), ~[2, 3]);
+		assert_eq!(c.to_vec(), ~[2, 3]);
 	}
 
 	#[test]
 	fn test_except_operator() {
-		let a = ::index::new( ~[1, 2, 3] );
-		let b = ::index::new( ~[2, 3, 4] );
+		let a = Index::new( ~[1, 2, 3] );
+		let b = Index::new( ~[2, 3, 4] );
 		let c = a - b;
-		assert_eq!(::index::to_vec(c), ~[1]);
+		assert_eq!(c.to_vec(), ~[1]);
 	}
 
 	#[test]
 	fn test_advanced() {
-		let a = ::index::new( ~[1, 5, 8] );
-		let b = ::index::new( ~[0, 1, 5] );
-		let c = ::index::new( ~[0, 1, 5, 8] );
+		let a = Index::new( ~[1, 5, 8] );
+		let b = Index::new( ~[0, 1, 5] );
+		let c = Index::new( ~[0, 1, 5, 8] );
 		let d = a + b - c;
-		assert_eq!(::index::to_vec(d), ~[]);
+		assert_eq!(d.to_vec(), ~[]);
 	}
 
 	#[test]
 	fn test_index_of_Index() {
-		let a = ::index::new( ~[1, 5, 10] );
-		let b = ::index::index_of(a, 5);
+		let a = Index::new( ~[1, 5, 10] );
+		let b = a.index_of(5);
 		assert_eq!(b, ::index::Found(1));
 	}
 
 	#[test]
 	fn test2_index_of_Index() {
-		let a = ::index::new( ~[1, 2, 10] );
-		let b = ::index::index_of(a, 0);
-		assert_eq!(b, ::index::Next(0));
+		let a = Index::new( ~[1, 2, 10] );
+		let b = a.index_of(0);
+		assert_eq!(b, ::index::FoundLarger(0));
 	}
 
 	#[test]
 	fn test_strs() {
-		let a = ::index::new( ~[~"apes", ~"banana", ~"monkey"] );
-		let b = ::index::new( ~[~"banana", ~"monkey", ~"snakes"] );
+		let a = Index::new( ~[~"apes", ~"banana", ~"monkey"] );
+		let b = Index::new( ~[~"banana", ~"monkey", ~"snakes"] );
 		let c = a + b;
-		assert_eq!(::index::to_vec(c), 
+		assert_eq!(c.to_vec(), 
 			~[~"apes", ~"banana", ~"monkey", ~"snakes"]);
 	}
 
 	#[test]
 	fn test_example_2() {
-		let a = ::index::new( ~[1, 2, 3] );
-		let b = ::index::new( ~[2, 3, 4] );
-		let c = ::index::new( ~[1, 4] );
+		let a = Index::new( ~[1, 2, 3] );
+		let b = Index::new( ~[2, 3, 4] );
+		let c = Index::new( ~[1, 4] );
 		let d = a * (b - c);
-		assert_eq!(::index::to_vec(d), ~[2, 3]);
+		assert_eq!(d.to_vec(), ~[2, 3]);
 	}
 }
 
